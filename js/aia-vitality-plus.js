6e7505dc338be243563613b77e08ac5c5e68f361
rathanont <rathanont.s@gmail.com>
2021-10-09 12:10:39 +0700

    update

--- js/aia-vitality-plus.js
+++ js/aia-vitality-plus.js
@@ -1,23 +1,27 @@
 $(function () {
+  // Prevent closing from click inside dropdown
+  $(document).on("click", ".dropdown-menu", function (e) {
+    e.stopPropagation();
+  });
 
-    // Prevent closing from click inside dropdown
-    $(document).on('click', '.dropdown-menu', function (e) {
-        e.stopPropagation();
+  // make it as accordion for smaller screens
+  if ($(window).width() < 992) {
+    $(".dropdown-menu a").click(function (e) {
+      e.preventDefault();
+      console.log("Click subtoggle");
+      if ($(this).next(".submenu").length) {
+        $(this).next(".submenu").toggle();
+      }
+      $(".dropdown").on("hide.bs.dropdown", function () {
+        $(this).find(".submenu").hide();
+      });
     });
+  }
 
-    // make it as accordion for smaller screens
-    if ($(window).width() < 992) {
-        $('.dropdown-menu a').click(function (e) {
-            e.preventDefault();
-            if ($(this).next('.submenu').length) {
-                $(this).next('.submenu').toggle();
-            }
-            $('.dropdown').on('hide.bs.dropdown', function () {
-                $(this).find('.submenu').hide();
-            })
-        });
-    }
+  $('[data-toggle="offcanvas"]').on("click", function () {
+    $(".offcanvas-collapse").toggleClass("open");
+  });
 
-    // Accepts any class name
-  var rellax = new Rellax('.rellax');
+  // Accepts any class name
+  var rellax = new Rellax(".rellax");
 });
